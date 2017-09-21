7289b74581b4947e37ac63a9963ae82455b9f39d
yhalk <elyiannis@gmail.com>
2017-09-21 14:25:23 +0000

    Implement low level behaviours

--- src/ev3/ir_to_control_ev3.py
+++ src/ev3/ir_to_control_ev3.py
@@ -6,6 +6,7 @@ import csv
 import os
 import low_level_ctrl as ctrl
 
+
 REMOTE_NONE = 0
 REMOTE_RED_UP = 1
 REMOTE_RED_DOWN = 2
@@ -25,35 +26,13 @@ def motor_control(actuators,cmd):
     speedB = 0
     print("motor_ctrl")
     if (cmd==REMOTE_RED_UP):
-        speedA  = -360
-        time_sp = 360
-        N = 1
-        ctrl.turn_N_steps(actuators[0],actuators[2],N=N,speed_sp=speedA,time_sp=time_sp)     
-    elif (cmd==REMOTE_RED_DOWN):
-        speedA  = 360
-        time_sp = 360
-        N = 1
-        ctrl.turn_N_steps(actuators[0],actuators[2],N=N,speed_sp=speedA,time_sp=time_sp)
+        ctrl.turn_18deg_step(actuators[0],actuators[2])     
     elif (cmd==REMOTE_BLUE_UP):
-        speedA  = -360
-        time_sp = 360
-        N = 1
-        ctrl.turn_N_steps(actuators[2],actuators[0],N=N,speed_sp=speedA,time_sp=time_sp)     
-    elif (cmd==REMOTE_BLUE_DOWN):
-        speedA  = 360
-        time_sp = 360
-        N = 1
-        ctrl.turn_N_steps(actuators[2],actuators[0],N=N,speed_sp=speedA,time_sp=time_sp)
+        ctrl.turn_18deg_step(actuators[2],actuators[0])     
     elif (cmd==REMOTE_RED_UP_AND_BLUE_UP):
-        speed_sp = 360
-        time_sp  = 360
-        N = 1
-        ctrl.forward_N_steps(actuators[2],actuators[0],N=N,speed_sp=speed_sp,time_sp=time_sp)
+        ctrl.forward_1_step_position(actuators[2],actuators[0],100)
     elif (cmd==REMOTE_RED_DOWN_AND_BLUE_DOWN):
-        speed_sp = 360
-        time_sp  = 360
-        N = 1
-        ctrl.backward_N_steps(actuators[2],actuators[0],N=N,speed_sp=speed_sp,time_sp=time_sp)
+        ctrl.backward_1_step_position(actuators[2],actuators[0],100)
     elif (cmd==REMOTE_BAECON_MODE_ON):
         ctrl.stop_actuator(actuators=[actuators[0],actuators[2]],stop_action='brake')
     else:
@@ -67,21 +46,13 @@ def gripper_control(actuators,cmd):
     speed_grip = 0
     print("gripper")
     if (cmd==REMOTE_RED_UP):
-        speed_lift = 250
-        position_sp = 70
-        ctrl.lift_gripper(actuator=actuators[1],position_sp=position_sp,speed_sp=speed_lift)
+        ctrl.lift_gripper_position(actuator=actuators[1],position=100)
     elif (cmd==REMOTE_RED_DOWN):
-        speed_lift = 250
-        position_sp = 70
-        ctrl.lower_gripper(actuator=actuators[1],position_sp=position_sp,speed_sp=speed_lift)
+        ctrl.lower_gripper_position(actuator=actuators[1],position=30)
     elif (cmd==REMOTE_BLUE_UP):
-        position_sp = 90
-        speed_sp = 200
-        ctrl.open_gripper(actuators[3],position=position_sp,speed_sp=speed_sp)
+        ctrl.open_gripper_position(actuators[3],position=100)
     elif (cmd==REMOTE_BLUE_DOWN):
-        position_sp = 90
-        speed_sp = 200
-        ctrl.close_gripper(actuators[3],position=position_sp,speed_sp=speed_sp)
+        ctrl.close_gripper_position(actuators[3],position=50)
     elif (cmd==REMOTE_BAECON_MODE_ON):
         ctrl.stop_actuator(actuators=[actuators[3],actuators[1]],stop_action='brake')
     else:
--- src/ev3/low_level_ctrl.py
+++ src/ev3/low_level_ctrl.py
@@ -1,4 +1,4 @@
-import os
+from ctrl_config import *
 
 def stop_actuator(actuators,stop_action):
 
@@ -6,92 +6,110 @@ def stop_actuator(actuators,stop_action):
         actuator.stop(stop_action=stop_action)
 
 #moving motors A,D
-def turn_N_steps(actuator1,actuator2,N,speed_sp=360,time_sp=360):
+
+def turn_18deg_step(actuator1,actuator2,speed_sp=SPEED_TURN,time_sp=TIME_TURN):
 #Turn robot 1 step, i.e. 18 degrees
-    for in in xrange(N):
-        actuator1.run_timed(time_sp=time_sp,speed_sp=speed_sp)
-        actuator2.run_timed(time_sp=time_sp,speed_sp=-speed_sp)
-    stop_actuator(actuators=[actuator1,actuator2],stop_action='brake')
 
-def forward_N_steps(actuator1,actuator2,N,speed_sp,time_sp):
-    
-    for i in xrange(N):
-        actuator1.run_timed(time_sp=time_sp,speed_sp=speed_sp)
-        actuator2.run_timed(time_sp=time_sp,speed_sp=speed_sp)
-    stop_actuator(actuators=[actuator1,actuator2],stop_action='brake')
+     actuator1.run_timed(time_sp=time_sp,speed_sp=speed_sp,stop_action='brake')
+     actuator2.run_timed(time_sp=time_sp,speed_sp=-speed_sp,stop_action='brake')
+
+
+def forward_1_step_time(actuator1,actuator2,speed_sp=SPEED_FWD,time_sp=TIME_FWD):
     
 
-def backward_N_steps(actuator1,actuator2,N,speed_sp,time_sp):
+    actuator1.run_timed(time_sp=time_sp,speed_sp=speed_sp,stop_action='brake')
+    actuator2.run_timed(time_sp=time_sp,speed_sp=speed_sp,stop_action='brake')
+    
 
-    for i in xrange(N):
-        actuator1.run_timed(time_sp=time_sp,speed_sp=-speed_sp)
-        actuator2.run_timed(time_sp=time_sp,speed_sp=-speed_sp)
-    stop_actuator(actuators=[actuator1,actuator2],stop_action='brake')
+
+def backward_1_step_time(actuator1,actuator2,speed_sp=SPEED_BWD,time_sp=TIME_BWD):
+
+    actuator1.run_timed(time_sp=time_sp,speed_sp=-speed_sp)
+    actuator2.run_timed(time_sp=time_sp,speed_sp=-speed_sp)
+    
+
+def forward_1_step_position(actuator1,actuator2,position,speed_sp=SPEED_FWD,time_sp=TIME_FWD):
+# 1 step is 3cm with position=100
+    actuator1.polarity = 'normal'
+    actuator2.polarity = 'normal'
+    actuator1.run_to_rel_pos(position_sp=position,speed_sp=speed_sp,stop_action='brake')
+    actuator2.run_to_rel_pos(position_sp=position,speed_sp=speed_sp,stop_action='brake')
+
+
+
+def backward_1_step_position(actuator1,actuator2,position,speed_sp=SPEED_BWD,time_sp=TIME_BWD):
+# 1 step is 3cm with position=100
+
+    actuator1.polarity = 'inversed'
+    actuator2.polarity = 'inversed'
+    actuator1.run_to_rel_pos(position_sp=position,speed_sp=speed_sp,stop_action='brake')
+    actuator2.run_to_rel_pos(position_sp=position,speed_sp=speed_sp,stop_action='brake')
     
 
 #manipulation motors C,D
-def open_gripper_position(actuator,position,speed_sp,time_sp=None):
+def open_gripper_position(actuator,position,speed_sp=SPEED_GRIP_OPEN,time_sp=None):
 
     print(actuator.position)
 
     actuator.polarity = 'normal'
     actuator.stop_action = 'hold'
-    actuator.run_to_abs_pos(position_sp=position,speed_sp=speed_sp)
+    actuator.run_to_rel_pos(position_sp=position,speed_sp=speed_sp)
 
     print(actuator.position)
      
 
-def close_gripper_position(actuator,position,speed_sp,time_sp=None):
+def close_gripper_position(actuator,position,speed_sp=SPEED_GRIP_CLOSE,time_sp=None):
 
     print(actuator.position)
 
-    actuator.polarity = 'inverse'
+    actuator.polarity = 'inversed'
     actuator.stop_action = 'hold'
-    actuator.run_to_abs_pos(position_sp=position,speed_sp=speed_sp)
+    actuator.run_to_rel_pos(position_sp=position,speed_sp=speed_sp)
     actuator.polarity = 'normal'
     
     print(actuator.position)
 
     
-def lift_gripper_position(actuator,position,speed_sp,time_sp=None):
+def lift_gripper_position(actuator,position,speed_sp=SPEED_GRIP_UP,time_sp=None):
 
     print(actuator.position)
 
     actuator.polarity = 'normal'
     actuator.stop_action = 'hold'
-    actuator.run_to_abs_pos(position_sp=position,speed_sp=speed_sp)
+    actuator.run_to_rel_pos(position_sp=position,speed_sp=speed_sp)
 
     print(actuator.position)
 
     
-def lower_gripper_position(actuator,position,speed_sp,time_sp=None):
+def lower_gripper_position(actuator,position,speed_sp=SPEED_GRIP_DOWN,time_sp=None):
 
     print(actuator.position)
 
-    actuator.polarity = 'inverse'
+    actuator.polarity = 'inversed'
     actuator.stop_action = 'hold'
-    actuator.run_to_abs_pos(position_sp=position,speed_sp=speed_sp)
+    actuator.run_to_rel_pos(position_sp=position,speed_sp=speed_sp)
     actuator.polarity = 'normal'
 
     print(actuator.position)
 
 
 #manipulation motors C,D
-def open_gripper_time(actuator,speed_sp,time_sp=None):
+def open_gripper_time(actuator,speed_sp=SPEED_GRIP_OPEN,time_sp=TIME_GRIP_OPEN):
 
     actuator.run_timed(time_sp=time_sp,speed_sp=speed_sp)
      
 
-def close_gripper_time(actuator,speed_sp,time_sp=None):
+def close_gripper_time(actuator,speed_sp=SPEED_GRIP_CLOSE,time_sp=TIME_GRIP_CLOSE):
 
     actuator.run_timed(time_sp=time_sp,speed_sp=speed_sp)
 
     
-def lift_gripper_time(actuator,speed_sp,time_sp=None):
-
-    actuator.run_timed(time_sp=time_sp,speed_sp=speed_sp)
+def lift_gripper_time(actuator,speed_sp=SPEED_GRIP_UP,time_sp=TIME_GRIP_UP):
 
     
-def lower_gripper_time(actuator,speed_sp,time_sp=None):
+    actuator.run_timed(time_sp=time_sp,speed_sp=speed_sp,stop_action='hold')
 
-    actuator.run_timed(time_sp=time_sp,speed_sp=speed_sp)
+    
+def lower_gripper_time(actuator,speed_sp=SPEED_GRIP_DOWN,time_sp=TIME_GRIP_UP):
+
+    actuator.run_timed(time_sp=time_sp,speed_sp=speed_sp,stop_action='hold')
